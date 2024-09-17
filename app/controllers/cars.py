3b178667ba5a2225369ccb342333179848a4269e
Felipe Oliveira <felpd3v@gmail.com>
2024-09-17 14:43:47 -0300

    feat: create a controller for `cars`

---
+++ app/controllers/cars.py
@@ -0,0 +1,30 @@
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.cars import CarCreate
+from app.models.cars import Car
+from uuid import UUID
+
+
+async def get_cars(db: AsyncSession):
+    result = await db.execute(select(Car))
+    return result.scalars().all()
+
+
+async def get_car(db: AsyncSession, car_id: UUID):
+    return await db.get(Car, car_id)
+
+
+async def create_car(db: AsyncSession, car: CarCreate):
+    db_car = Car(**car.model_dump())
+    db.add(db_car)
+    await db.commit()
+    await db.refresh(db_car)
+    return db_car
+
+
+async def delete_car(db: AsyncSession, car_id: int):
+    db_car = await get_car(db, car_id)
+    if db_car:
+        await db.delete(db_car)
+        await db.commit()
+    return db_car
