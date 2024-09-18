3c6f967afeef7e80fb9b3946c6f6bd1302800dff
Felipe Oliveira <felpd3v@gmail.com>
2024-09-18 10:52:25 -0300

    chore: use database url from os env

--- core/config.py
+++ core/config.py
@@ -1,18 +1,8 @@
 import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
 
 
 class Settings:
-    DB_USER: str = os.getenv("POSTGRES_USER", "")
-    DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "")
-    DB_HOST: str = os.getenv("POSTGRES_HOST", "")
-    DB_PORT: str = os.getenv("POSTGRES_PORT", "")
-    DB_NAME: str = os.getenv("POSTGRES_DB", "")
-
-    DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{
-        DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 
 
 settings = Settings()
--- core/database.py
+++ core/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import settings
+# DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/car_database"
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/car_database"
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
