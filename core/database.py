94cef691c45c6d5d8c667176db9a770adf7953b1
Felipe Oliveira <felpd3v@gmail.com>
2024-09-18 10:52:56 -0300

    chore: remove comments

--- core/database.py
+++ core/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
-# DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/car_database"
 
 engine = create_async_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(
